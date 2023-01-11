3b5d1f9972a680f2a70aa278cc7194c90d2753c5
Celiancaf <104433371+Celiancaf@users.noreply.github.com>
2023-01-11 17:53:22 +0100

    (SA) EmptySolverRecuit

---
+++ Sudoku.Recuit/EmptySolverRecuit.cs
@@ -0,0 +1,18 @@
+﻿using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Recuit
+{
+    public class EmptySolverRecuit : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+
+    }
+}
