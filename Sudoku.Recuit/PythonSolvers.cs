0dc2340567922f403c3c4964d47db8d8faa3fc13
Celiancaf <104433371+Celiancaf@users.noreply.github.com>
2023-01-16 10:39:22 +0100

    Update PythonSolvers

--- Sudoku.Recuit/PythonSolvers.cs
+++ Sudoku.Recuit/PythonSolvers.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+using Sudoku.Shared;
+
+namespace Sudoku.Recuit
+{
+
+
+
+
+    public class RecuitPythonDotNetSolver : PythonSolverBase
+    {
+
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+            //System.Diagnostics.Debugger.Break();
+
+            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pySudoku = s.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("sudoku", pySudoku);
+
+                // the person object may now be used in Python
+                string code = Resources.SelfCallSolver_py;
+                scope.Exec(code);
+                var result = scope.Get("solvedSudoku");
+                var toReturn = result.As<Shared.SudokuGrid>();
+                return toReturn;
+            }
+            //}
+
+        }
+
+    }
+
+    public class RecuitPythonNativeSolver : PythonSolverBase
+    {
+
+
+        public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+        {
+
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pyCells = s.Cells.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("instance", pyCells);
+
+                // the person object may now be used in Python
+                string code = Resources.RecuitSolver_py;
+                scope.Exec(code);
+                var result = scope.Get("r");
+                var managedResult = result.As<int[][]>();
+                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+                return new Shared.SudokuGrid() { Cells = managedResult };
+            }
+            //}
+
+        }
+
+        protected override void InitializePythonComponents()
+        {
+            InstallPipModule("recuit-solver");
+            base.InitializePythonComponents();
+        }
+
+
+
+    }
+
+
+}
