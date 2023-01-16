18f712062fdbb03f6653e1174721810cf5a75bac
alckh <annelaure.chukoyeho@edu.ece.fr>
2023-01-16 10:02:54 +0100

    SelfCallSolver update name 2

--- Sudoku.Recuit/Ressources/SelfCallSolver.py
+++ Sudoku.Recuit/Ressources/SelfCallSolver.py
@@ -1,6 +1,6 @@
 import clr
 clr.AddReference("Sudoku.Shared")
 clr.AddReference("Sudoku.SodukuRecuitSolver")
-from Sudoku.RecuitSolver import RecuitSubstitutionsSolver
-netSolver = RecuitSubstitutionsSolver()
+from Sudoku.RecuitSolver import SudokuRecuitSolver
+netSolver = SudokuRecuitSolver()
 solvedSudoku = netSolver.Solve(sudoku)
