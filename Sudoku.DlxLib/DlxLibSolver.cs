007efcf2c3da26bad301a0837c165ca6396c610f
Jean-Sylvain Boige <jsboige@omnesintervenant.com>
2023-01-17 16:40:49 +0100

    nettoyage premier solver vide dlx

--- Sudoku.DlxLib/DlxLibSolver.cs
+++ Sudoku.DlxLib/DlxLibSolver.cs
@@ -4,6 +4,7 @@ public class DlxLibSolver:ISudokuSolver
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
+            //A faire: récupérer les cellules du Masque de sudoku à résoudre dans s.Cells et utiliser le code fourni dans le sujet pour résoudre le sudoku 
             return s.CloneSudoku();
         }
 }
