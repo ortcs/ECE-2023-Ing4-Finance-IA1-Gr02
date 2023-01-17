636f347e8ba3552a877643597240d532a295d27c
Jean-Sylvain Boige <jsboige@omnesintervenant.com>
2023-01-17 17:00:32 +0100

    Instructions après solver génétique vide

--- Sudoku.Genetic/GeneticSolver.cs
+++ Sudoku.Genetic/GeneticSolver.cs
@@ -5,6 +5,8 @@ public class GeneticSolver: ISudokuSolver
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
+            //A faire: j'ai rajouter les packages GeneticSharp et GeneticSharp.Extensions,
+            //il devrait être possible de reprendre une bonne partie du code documenté notamment dans les tests unitaire de GeneticSharp 
             return s.CloneSudoku();
         }
 
