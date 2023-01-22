e4c4a6d0938c666d04bb84a3219719eb41735432
SofiaGADACHA <sofia.gadachacharrad@edu.ece.fr>
2023-01-22 16:32:52 +0100

    Installation geneticSharp

--- Sudoku.Genetic/GeneticSolver.cs
+++ Sudoku.Genetic/GeneticSolver.cs
@@ -1,13 +1,17 @@
 ﻿using Sudoku.Shared;
-
 namespace Sudoku.Genetic;
+
 public class GeneticSolver: ISudokuSolver
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
+            
+          
+
             //A faire: j'ai rajouter les packages GeneticSharp et GeneticSharp.Extensions,
             //il devrait être possible de reprendre une bonne partie du code documenté notamment dans les tests unitaire de GeneticSharp 
             return s.CloneSudoku();
         }
 
     }
+
