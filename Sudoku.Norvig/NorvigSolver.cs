5865133b3deab58b8fe8ef8a63b2fed6389d77e1
ThomasLaplaud <thomas.laplaud@edu.ece.fr>
2023-01-24 10:22:48 +0100

    solver Norvig vide

---
+++ Sudoku.Norvig/NorvigSolver.cs
@@ -0,0 +1,11 @@
+using Sudoku.Shared;
+namespace Sudoku.Norvig;
+
+public class NorvigSolver: ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+
+    }
