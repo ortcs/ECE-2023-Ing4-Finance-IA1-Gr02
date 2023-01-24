4eef54001703d98abada8b6ab63d7be07db61ca8
Ayoub-mtn <72629595+Ayoub-mtn@users.noreply.github.com>
2023-01-24 09:51:03 +0100

    emptySolver

---
+++ Sudoku.Norvig/NorvigSolver.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+
+namespace Sudoku.Norvig
+{
+    public class NorvigSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
