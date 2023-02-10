a0483b98eb01b1e69124805a2b6b61d8799e2786
Ayoub-mtn <72629595+Ayoub-mtn@users.noreply.github.com>
2023-02-10 12:12:38 +0100

    solution norvig solver

--- Sudoku.Norvig/NorvigSolver.cs
+++ Sudoku.Norvig/NorvigSolver.cs
@@ -7,24 +7,30 @@ using System.Linq;
 
 namespace Sudoku.Norvig
 {
-    public class NorvigSolver : ISudokuSolver
-    {
-        public SudokuGrid Solve(SudokuGrid s)
-        {
-            var sudokuString = s.Cells.Select(row=>row.Select(cell=>cell==0?".":cell.ToString(CultureInfo.InvariantCulture)).Aggregate((s1,s2)=>s1+s2)).Aggregate((s1,s2)=>s1+s2);
-            var linqGrid = LinqSudokuSolver.parse_grid(sudokuString);
-            var solved = LinqSudokuSolver.search(linqGrid);
-            for(int i=1;i<9;i++)
-            {
-                for(int c=1;c<9;c++)
-                {
-                    s.Cells[i][c]=int.Parse(solved[((char)('A'+i))+c.ToString(CultureInfo.InvariantCulture)]);
-                }
-            }
-            return s;
+	public class NorvigSolver : ISudokuSolver
+	{
+		public SudokuGrid Solve(SudokuGrid s)
+		{
+			var sudokuString = s.Cells.Select(row => row.Select(cell => cell == 0 ? "." : cell.ToString(CultureInfo.InvariantCulture)).Aggregate((s1, s2) => s1 + s2)).Aggregate((s1, s2) => s1 + s2);
+			var linqGrid = LinqSudokuSolver.parse_grid(sudokuString);
+			var solved = LinqSudokuSolver.search(linqGrid);
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 1; j < 10; j++)
+				{
+					var key = ((char)('A' + i)) + j.ToString(CultureInfo.InvariantCulture);
+					var strCellValue = solved[key];
+					s.Cells[i][j-1] = int.Parse(strCellValue);
+				}
+			}
+			return s;
 
-        }
-    }
+		}
+	}
+
+
+
+    
 
 
 
